a432d3f321988529cf69ab824167a8405aefcdeb
Sara <sara@saratonin.dev>
2022-03-07 18:13:16 -0500

    prettier to read

--- src/Day1.hs
+++ src/Day1.hs
@@ -8,7 +8,7 @@ part1 x = fst $ foldl part1' (0, -1) x
 -- increases, prev_depth
 part1' :: (Int, Int) -> Int -> (Int, Int) 
 part1' (acc, -1) newDepth = (acc, newDepth)
-part1' (acc, prevDepth) newDepth = (acc + if prevDepth < newDepth then 1 else 0, newDepth)
+part1' (acc, prevDepth) newDepth = (if prevDepth < newDepth then succ acc else acc, newDepth)
 
 part2 :: [Int] -> Int
 part2 ls = fst $ foldl part1' (0, -1) $ map sum $ Utils.windows ls 3

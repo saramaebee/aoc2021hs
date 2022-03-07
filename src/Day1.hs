8f61a01c27ff801a61d9596f58c20448da70c04b
Sara <sara@saratonin.dev>
2022-03-07 18:18:50 -0500

    adding Maybe

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,17 +1,17 @@
 module Day1 (run) where
 
-import Utils (parseIntsFromStrings, readInputFile)
+import Utils (parseIntsFromStrings, windows,  readInputFile)
 
 part1 :: [Int] -> Int
-part1 x = fst $ foldl part1' (0, -1) x
+part1 x = fst $ foldl part1' (0, Nothing) x
 
 -- increases, prev_depth
-part1' :: (Int, Int) -> Int -> (Int, Int) 
-part1' (acc, -1) newDepth = (acc, newDepth)
-part1' (acc, prevDepth) newDepth = (if prevDepth < newDepth then succ acc else acc, newDepth)
+part1' :: (Int, Maybe Int) -> Int -> (Int, Maybe Int) 
+part1' (acc, Nothing) newDepth = (acc, Just newDepth)
+part1' (acc, Just prevDepth) newDepth = (if prevDepth < newDepth then succ acc else acc, Just newDepth)
 
 part2 :: [Int] -> Int
-part2 ls = fst $ foldl part1' (0, -1) $ map sum $ Utils.windows ls 3
+part2 ls = fst $ foldl part1' (0, Nothing) $ map sum $ Utils.windows ls 3
 
 run :: [String] -> (Int, Int)
 run i' = (let i = parseIntsFromStrings i' in (part1 i, part2 i)) 

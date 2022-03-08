37c96f1aaf1c3487bac4098546efc0107a63f474
Sara <sara@saratonin.dev>
2022-03-07 21:11:07 -0500

    day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -3,51 +3,46 @@ module Day2 where
 import Utils (readInt, readInputFile)
 
 run :: [String] -> (Int, Int)
-run l = (part1 l, -1)
+run l = (part1 l', part2 l') where 
+	l' = fmap parseCommand l
 
+part1 :: [Command] -> Int
+part1 c = d * hP where (d, hP, _) = foldl runCommand (0, 0, Nothing) c
+
+part2 :: [Command] -> Int
+part2 c = d * hP where (d, hP, a) = foldl runCommand (0, 0, Just 0) c 
+
+type Depth = Int
+type HorizonalPosition = Int
+type Aim = Int
+data Direction = Forward | Up | Down 
+type Length = Int
+type Position = (Depth, HorizonalPosition, Maybe Aim)
+type Command = (Direction, Length)
+
+runCommand :: Position -> Command -> Position
 -- part 1
-type Pos = (Int, Int) -- Depth, HorPoz 
-data Dir = Down | Up | Forward 
-data Dir' = Depth | HorPoz
-type ChPosReq = (Int, Dir) -- Down, Up, Forward
-type ChPosReq' = (Int, Dir') -- Depth, HorPoz
+runCommand (d, h, Nothing) (Forward, len) = (d, h + len, Nothing)
+runCommand (d, h, Nothing) (Up, len) = (d - len, h, Nothing) 
+runCommand (d, h, Nothing) (Down, len) = (d + len, h, Nothing)
 
-part1' :: Pos -> [String] -> Pos 
-part1' p [] = p 
-part1' p (c:cs) = let s = changePos p (parseRequest (parseCommand c)) in part1' s cs
+-- part 2
+runCommand (d, h, Just a) (Forward, len) = (d + (a * len), h + len, Just a)
+runCommand (d, h, Just a) (Up, len) = (d, h, Just $ a - len) 
+runCommand (d, h, Just a) (Down, len) = (d, h, Just $ a + len)
 
-part1 :: [String] -> Int 
-part1 c = let t = part1' (0, 0) c in fst t * snd t
 
-changePos :: Pos -> ChPosReq' -> Pos
-changePos (d, h) (v, Depth) = (d+v, h)
-changePos (d, h) (v, HorPoz) = (d, h+v) 
 
-parseRequest :: ChPosReq -> ChPosReq'
-parseRequest (n, Forward) = (n, HorPoz)
-parseRequest (n, Down) = (n, Depth)
-parseRequest (n, Up) = (-n, Depth)
+parseCommand :: String -> Command
+parseCommand s = (d, l) where
+	(d':l':_) = words s
+	d = parseDirection d' 
+	l = parseLength l' 
 
-parseCommand :: String -> ChPosReq
-parseCommand s = let c = parseCommand' s in (snd c, parseDir (fst c))
+parseDirection :: String -> Direction
+parseDirection "forward" = Forward
+parseDirection "up" = Up
+parseDirection "down" = Down
 
-parseCommand' :: String -> (String, Int)
-parseCommand' s = let m = words s in (head m, readInt (head (tail m)))
-
-parseDir :: String -> Dir
-parseDir "forward" = Forward 
-parseDir "down" = Down 
-parseDir "up" = Up
-
--- part 2 - WIP
--- type Position = (Int, Int, Int) -- Aim, Depth, HorPoz
--- type ChangePositionRequest = (Int, Int, Dir)-- Aim, Distance, Direction
-
--- changePos' :: Position -> ChangePositionRequest -> Position
--- changePos' (aim, direction, horPoz) (aim', distance', direction') 
-
--- parseRequest' :: String -> ChangePositionRequest
--- parseRequest' s = let c = words s in 
-
--- parseStringListToCommand :: [String] -> ChangePositionRequest
--- parseStringListToCommand ("forward":ls) = (0,...) -- and here is where i got stuck
+parseLength :: String -> Length
+parseLength s = read s
